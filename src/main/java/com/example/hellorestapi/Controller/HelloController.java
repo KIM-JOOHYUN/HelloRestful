865102730a685f77bbd0dddbfb2572c6096b6ec2
KIM-JOOHYUN <kjh99723@gmail.com>
2022-10-20 18:53:36 +0900

    Change Test

--- src/main/java/com/example/hellorestapi/Controller/HelloController.java
+++ src/main/java/com/example/hellorestapi/Controller/HelloController.java
@@ -2,6 +2,7 @@ package com.example.hellorestapi.Controller;
 
 import com.example.hellorestapi.Service.Hello;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +16,10 @@ public class HelloController {
     @RequestMapping("/hello")
     public String hello(@RequestParam("user") String user) {
         return hello.hello(user);
+    }
+
+    @GetMapping("/helloEvery")
+    public String helloEvery() {
+        return "Hello Everyone!";
     }
 }
